032893b3d8e1bfbb48d42d5cc01dbcbba9e2cb24
Levi Oliveira <lev.developersr1@gmail.com>
2025-12-07 18:36:04 -0300

    Criando modelo do contexto

---
+++ arquiteturaspring/src/main/java/io/github/Leiv/arquiteturaspring/montadora/Montadora.java
@@ -0,0 +1,6 @@
+package io.github.Leiv.arquiteturaspring.montadora;
+
+public enum Montadora {
+    HONDA,
+    TOYOTA
+}
---
+++ arquiteturaspring/src/main/java/io/github/Leiv/arquiteturaspring/montadora/TipoMotor.java
@@ -0,0 +1,7 @@
+package io.github.Leiv.arquiteturaspring.montadora;
+
+public enum TipoMotor {
+    ASPIRADO,
+    TURBO,
+    ELETRICO
+}
